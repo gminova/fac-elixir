1e652e4ff27488ece7e59cd9b6d713b7c7f45581
Gigi Minova <minova.gigi@gmail.com>
2019-10-21 14:43:20 +0100

    add more tests

--- test/fac_test.exs
+++ test/fac_test.exs
@@ -14,4 +14,14 @@ defmodule FacTest do
     #The pipe operator which feeds value in function after it
     assert Fac.map([], fn _add = x -> x * x end) |> is_list()
   end
+
+  test "check map function returns correct length" do
+    #The pipe operator which feeds value in function after it
+    assert Fac.map([1, 2, 3], fn _add = x -> x * x end) |> length() == 3
+  end
+
+  test "check map function returns correct values in list" do
+    #Works similar to deepEqual since it compares actual values but not references
+    assert Fac.map([1, 2, 3], fn _add = x -> x * x end) == [1, 4, 9]
+  end
 end
